27f7123b2f0cf6dcf0c72903efa5cab33be673b2
Julio Chazari <julio.chazari@gmail.com>
2020-05-21 19:49:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_english_meaning(path, emoticon)
   emoticons_library = load_library(path)
-  english_meaning = emoticons_library.each do |key, value|
+  english_meaning = emoticons_library.map do |key, value|
     value.each do |inner_key, inner_value|
       if inner_value == emoticon
         key
