6a5912080dec7774d5638dfb12c8b89b57d4b500
Julio Chazari <julio.chazari@gmail.com>
2020-05-21 20:00:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_english_meaning(path, emoticon)
   emoticons_library = load_library(path)
-  english_meaning = ""
+  english_meaning = "Sorry, that emoticon was not found"
   emoticons_library.each do |key, value|
     value.each do |inner_key, inner_value|
       if inner_value == emoticon
