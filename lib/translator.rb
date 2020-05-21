eb7b2c5200465293d144a922b33f7080fe4a5961
Julio Chazari <julio.chazari@gmail.com>
2020-05-21 19:05:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@ require 'pry'
 require 'yaml'
 
 def load_library(library)
-  fixed_emoticons_hash = YAML.load_file(library).map do |emotion, emoticons|
+  YAML.load_file(library).map do |emotion, emoticons|
     some_hash = {}
     some_other_var = {:english => emoticons[0], :japanese => emoticons[1]}
     some_hash[emotion] = some_other_var
