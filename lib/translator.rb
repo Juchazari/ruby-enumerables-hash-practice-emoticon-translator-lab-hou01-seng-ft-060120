da1c4d7e3c5e1d4dcce41f684c17568f2c6e6174
Julio Chazari <julio.chazari@gmail.com>
2020-05-21 18:51:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,8 @@ require 'yaml'
 def load_library(library)
   fixed_emoticons_hash = YAML.load_file(library).each do |emotion, emoticons|
     emoticons.to_h
+    binding.pry
   end
-  binding.pry
 end
 
 def get_japanese_emoticon
